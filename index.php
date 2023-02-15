85eb9639cd253b8cb59bcb12deba333ca3234d64
Gaurav gupta <Gaurav.gupta@innoraft.com>
2023-02-15 17:13:23 +0530

    FT2023-72: Completed tasks 3 with oops concept.

--- index.php
+++ index.php
@@ -1,59 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  First Name: <input type="text" name="firstName" pattern="[a-zA-Z]{1,}" required><br><br>
-  Last Name: <input type="text" name="lastName" pattern="[a-zA-Z]{1,}" required><br><br>
-  Full name: <input type="text" name="fullName" value="<?php echo $fullName; ?>" disabled><br><br>
-  Select image:<input type="file" name="img" accept="image/*"><br><br>
-  Marks:<br><textarea name="marks" rows="5" cols="30"></textarea><br><br>
-  <input type="submit" value="Submit">
+<html>
+  <head>
+    <title>Form Example</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="index.js"></script>
     
-</form>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $firstName = $_POST["firstName"];
-  $lastName = $_POST["lastName"];
-  $fullName = $firstName . " " . $lastName;
-  $file_name= $_FILES['img']['name'];
-  $file_tmp = $_FILES['img']['tmp_name'];
-  move_uploaded_file($file_tmp,"upload-images".$file_name);
-  echo "<p><img src='upload-images$file_name' alt='img'></p>";
-  echo "Hello " . $fullName;
-
-  
-  $marks = $_POST['marks'];
-
-  $marks_array = explode("\n", $marks);
-
-  
-  ?>
-  <table border="1">
+  </head>
+  <body>
+    <form method="post" enctype="multipart/form-data">
+      <label for="first-name">First Name:</label>
+      <input type="text" id="first-name" name="first-name"  required>
+      <br>
+      <label for="last-name">Last Name:</label>
+      <input type="text" id="last-name" name="last-name" required>
+      <br>
+      <label for="full-name">Full Name:</label>
+      <input type="text" id="full-name" name="full-name" disabled>
+      <br>
+      <label for="img">Select image:</label>
+       <input type="file" id ="img" name="img" accept="image/*">
+      <br><br>
+      Marks:<br><textarea name="marks" rows="5" cols="30"></textarea><br><br>
+      <button type="submit">Submit</button>
+    </form>
+    <?php 
+      include 'logic.php'; 
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    ?>
+    <table border="1">
+      <thead>  
       <tr>
         <th>Subject</th>
         <th>Marks</th>
       </tr>
-  <?php    
-  foreach ($marks_array as $mark) {
-  $subject_mark = explode("|", $mark);
-  ?>
+</thead>
+  <?php for ($i=0;$i<$j;$i++) {?>
   <tr>
-    <td><?php echo $subject_mark[0]; ?></td>
-    <td><?php echo $subject_mark[1]; ?></td>
+    <td><?php echo $subject_mark[$i]; ?></td>
+    <td><?php echo $subject_mark[$i=$i+1]; ?></td>
   </tr>
-<?php
-}
-?>
+<?php }} ?>
 </table>
-<?php
-}
-?>
-</body>
+  </body>
 </html>
