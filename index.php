abe616209522d818013af9a5d9604d791b2e1a63
Gaurav gupta <Gaurav.gupta@innoraft.com>
2023-02-15 19:28:33 +0530

    FT2023-72: Completed task4 with oops concept.

--- index.php
+++ index.php
@@ -1,64 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  First Name: <input type="text" name="firstName" pattern="[a-zA-Z]{1,}" required><br><br>
-  Last Name: <input type="text" name="lastName" pattern="[a-zA-Z]{1,}" required><br><br>
-  Full name: <input type="text" name="fullName" value="<?php echo $fullName; ?>" disabled><br><br>
-  Select image:<input type="file" name="img" accept="image/*"><br><br>
-  Marks:<br><textarea name="marks" rows="5" cols="30"></textarea><br><br>
-  Phone Number: <input type="text" value="+91" name="number"  maxlength="13"><br><br>
-  <input type="submit" value="Submit">
+<html>
+  <head>
+    <title>Form Example</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="index.js"></script>
     
-</form>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $firstName = $_POST["firstName"];
-  $lastName = $_POST["lastName"];
-  $fullName = $firstName . " " . $lastName;
-  $file_name= $_FILES['img']['name'];
-  $file_tmp = $_FILES['img']['tmp_name'];
-  $marks = $_POST['marks'];
-  $number = $_POST['number'];
-  $marks_array = explode("\n", $marks);
-
-  move_uploaded_file($file_tmp,"upload-images".$file_name);
-  echo "<p><img src='upload-images$file_name' alt='img'></p>";
-  echo "Hello " . $fullName . "<br>";
-  if (preg_match("/^\+91[0-9]{10}$/", $number)) {
-    echo "Your phone number" . $number . "<br>";
-  } else {
-
-    echo "Invalid phone number. Please enter a valid Indian phone number in the format +91xxxxxxxxxx.";
-  }
-
-  ?>
-  <table border="1">
+  </head>
+  <body>
+    <form method="post" enctype="multipart/form-data">
+      <label for="first-name">First Name:</label>
+      <input type="text" id="first-name" name="first-name"  required>
+      <br>
+      <label for="last-name">Last Name:</label>
+      <input type="text" id="last-name" name="last-name" required>
+      <br>
+      <label for="full-name">Full Name:</label>
+      <input type="text" id="full-name" name="full-name" disabled>
+      <br>
+      <label for="img">Select image:</label>
+       <input type="file" id ="img" name="img" accept="image/*">
+      <br><br>
+      Marks:<br><textarea name="marks" rows="5" cols="30" required></textarea><br><br>
+      Phone Number: <input type="text" value="+91" name="number"  maxlength="13" required><br><br>
+      <button type="submit">Submit</button>
+    </form>
+    <?php 
+      include 'logic.php'; 
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    ?>
+    <table border="1">
+      <thead>  
       <tr>
         <th>Subject</th>
         <th>Marks</th>
       </tr>
-  <?php    
-  foreach ($marks_array as $mark) {
-  $subject_mark = explode("|", $mark);
-  ?>
+</thead>
+  <?php for ($i=0;$i<$j;$i++) {?>
   <tr>
-    <td><?php echo $subject_mark[0]; ?></td>
-    <td><?php echo $subject_mark[1]; ?></td>
+    <td><?php echo $subject_mark[$i]; ?></td>
+    <td><?php echo $subject_mark[$i=$i+1]; ?></td>
   </tr>
-<?php
-}
-?>
+<?php }} ?>
 </table>
-<?php
-}
-?>
-</body>
+  </body>
 </html>
---
+++ logic.php
@@ -0,0 +1,46 @@
+<?php
+class form{
+  public $first,$last,$full,$filename,$filetemp;
+    
+  public function __construct($firstName,$lastName,$file_name,$file_temp){
+    $this->first=$firstName;
+    $this->last=$lastName;
+    $this->filename=$file_name;
+    $this->filetemp=$file_temp;
+  }
+  public function showfullname(){
+    $message = "Hello " . $this->full = $this->first . " " . $this->last . "<br><br>";
+    echo $message;
+  }
+
+  public function uploadimage($file_name){
+    move_uploaded_file($this->filetemp,"upload-images/$this->filename");
+    echo "<p><img src='upload-images/$file_name' alt='img'></p>";
+  }
+  public function showmarks($marks){
+    $pattern = "/[\n\|]+/";
+    $subject_mark = preg_split($pattern, $marks);
+    return $subject_mark;
+  }
+}
+ 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $firstName = $_POST["first-name"];
+  $lastName = $_POST["last-name"];
+  $file_name= $_FILES['img']['name'];
+  $file_tmp = $_FILES['img']['tmp_name'];
+  $marks = $_POST['marks'];
+  $name = "/^[a-zA-Z]+$/";
+  $number = $_POST['number'];
+  if (preg_match($name, $firstName) && preg_match($name, $lastName) && (preg_match("/^\+91[0-9]{10}$/", $number))) {
+    $task = new form($firstName,$lastName,$file_name,$file_tmp);
+    $task->uploadimage($file_name);
+    $task->showfullname();   
+    $subject_mark = $task->showmarks($marks);
+    $j=count($subject_mark);
+    echo "Your phone number" . $number . "<br>";
+    } 
+    else{
+      echo "Enter all fields correctly;";
+    }
+  }
