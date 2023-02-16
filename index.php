4d04a43e1802fda1b385fd0090f0dd871ba04d74
Gaurav gupta <Gaurav.gupta@innoraft.com>
2023-02-16 11:09:30 +0530

    FT2023-72: Add validation semantic and styling in task4.

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
+<!DOCTYPE html>
 <html>
   <head>
     <title>Form Example</title>
+    <!--include style.css for styling-->
+    <link rel="stylesheet" href="style.css">
+    <!-- include jQuery library -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- include index.js for scripting -->
     <script src="index.js"></script>
-    
   </head>
   <body>
+    <!-- create a form with two input fields for first and last name, image,marks,phone number and a button to submit the form -->
     <form method="post" enctype="multipart/form-data">
       <label for="first-name">First Name:</label>
       <input type="text" id="first-name" name="first-name"  required>
@@ -17,17 +22,26 @@
       <input type="text" id="full-name" name="full-name" disabled>
       <br>
       <label for="img">Select image:</label>
-       <input type="file" id ="img" name="img" accept="image/*">
+       <input type="file" id ="img" name="img" accept="image/*" required>
       <br><br>
-      Marks:<br><textarea name="marks" rows="5" cols="30" required></textarea><br><br>
-      Phone Number: <input type="text" value="+91" name="number"  maxlength="13" required><br><br>
+      <label for="marks" id="marks">Marks:</label>
+      <textarea name="marks" rows="5" cols="30" required></textarea><br><br>
+      <label for="phone">Phone:</label>
+      <select id="country-code" name="country-code">
+      <option value="+91">+91</option>
+      <option value="+81">+81</option>
+      </select>
+      <input type="text" id="phone" name="phone" pattern="\d{10}" title="Please enter a valid Indian phone number starting with +91 and 10 digits long." required><br><br>
+
       <button type="submit">Submit</button>
     </form>
     <?php 
+      //include logic.php to handle form submission     
       include 'logic.php'; 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ?>
-    <table border="1">
+    <!-- Make a table for submission of value -->
+    <table>
       <thead>  
       <tr>
         <th>Subject</th>
--- logic.php
+++ logic.php
@@ -1,46 +1,80 @@
 <?php
-class form{
-  public $first,$last,$full,$filename,$filetemp;
-    
-  public function __construct($firstName,$lastName,$file_name,$file_temp){
-    $this->first=$firstName;
-    $this->last=$lastName;
-    $this->filename=$file_name;
-    $this->filetemp=$file_temp;
+// Define a class to represent the form
+class Form {
+  public $first, $last, $full, $filename, $filetemp, $phone;
+  //constructor to input data
+  public function __construct($firstName, $lastName, $file_name, $file_temp,$phone) {
+    $this->first = $firstName;
+    $this->last = $lastName;
+    $this->filename = $file_name;
+    $this->filetemp = $file_temp;
+    $this->phone = $phone;
   }
-  public function showfullname(){
-    $message = "Hello " . $this->full = $this->first . " " . $this->last . "<br><br>";
+
+  // Function to display the full name
+  public function showFullName() {
+    $message = "Hello " . $this->full = $this->first . " " . $this->last;
     echo $message;
   }
 
-  public function uploadimage($file_name){
-    move_uploaded_file($this->filetemp,"upload-images/$this->filename");
-    echo "<p><img src='upload-images/$file_name' alt='img'></p>";
+  // Function to upload the image
+  public function uploadImage() {
+    // Validate if image is selected and not empty
+    if (!empty($this->filename) && !empty($this->filetemp)) {
+      move_uploaded_file($this->filetemp, "upload-images/$this->filename");
+    }
   }
-  public function showmarks($marks){
+  //Function to split string into array
+  public function splitMarks($marks){
     $pattern = "/[\n\|]+/";
     $subject_mark = preg_split($pattern, $marks);
     return $subject_mark;
   }
+  //Function to print phone number
+  public function phoneNumber(){
+    echo "<br>+91" . $this->phone;
+  }
+
 }
  
+//Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  //insert the value
   $firstName = $_POST["first-name"];
   $lastName = $_POST["last-name"];
-  $file_name= $_FILES['img']['name'];
+  $file_name = $_FILES['img']['name'];
   $file_tmp = $_FILES['img']['tmp_name'];
   $marks = $_POST['marks'];
-  $name = "/^[a-zA-Z]+$/";
-  $number = $_POST['number'];
-  if (preg_match($name, $firstName) && preg_match($name, $lastName) && (preg_match("/^\+91[0-9]{10}$/", $number))) {
-    $task = new form($firstName,$lastName,$file_name,$file_tmp);
-    $task->uploadimage($file_name);
-    $task->showfullname();   
-    $subject_mark = $task->showmarks($marks);
-    $j=count($subject_mark);
-    echo "Your phone number" . $number . "<br>";
-    } 
-    else{
-      echo "Enter all fields correctly;";
-    }
+  $phone = $_POST['phone'];
+
+  // make variable namePattern to match only alphabets
+  $namePattern = "/^[a-zA-Z]+$/";
+
+  // make variable namePattern to match only alphabets
+  $marksPattern = '/^[a-zA-Z]+\|[0-9]+$/m';
+
+  // check if the first name and last name and marks match the pattern  
+  if (preg_match($namePattern, $firstName) && preg_match($namePattern, $lastName) && preg_match($marksPattern,$marks)) {
+
+    // Create a new form instance
+    $task = new Form($firstName,$lastName,$file_name,$file_tmp,$phone);
+    
+    // if they match, upload image using the method
+    $task->uploadImage($file_name);
+    
+    // print the image
+     echo "<p><img src='upload-images/$file_name' alt='img'></p>";
+    
+    // if they match, show using method
+    $task->showFullName();  // for name 
+    $subject_mark = $task->splitMarks($marks);// for marks
+    $j=count($subject_mark);// for count arraylength
+    $task->phoneNumber();
   }
+  else 
+  { 
+    // if they dont match, show the error message using the method
+    echo "Invalid input. Please enter only alphabets for first name and last name or provide marks in specified format";
+  }
+}
+
