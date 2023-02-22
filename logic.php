7f013c1a3edc5d58e66e978c964a2e80d3fa2679
Gaurav gupta <Gaurav.gupta@innoraft.com>
2023-02-22 09:54:19 +0530

    FT2023-72: Add comments and space as per drupal coding standards

--- logic.php
+++ logic.php
@@ -5,61 +5,62 @@
  * Represents a form with inputs
  */
 class Form {
-    /** @var string $first The first name input */
-    public $first;
-    /** @var string $last The last name input */
-    public $last;
-    /** @var string $full The full name concatenated from first and last names */
-    public $full;
-       
-    /**
-     * Constructor to initialize the first and last name
-     *
-     * @param string $firstName
-     * @param string $lastName
-     * 
-     * @return void
-     */
-    public function __construct($firstName, $lastName){
-      $this->first = $firstName;
-      $this->last = $lastName;
-    }
+  /** @var string $first The first name input */
+  public $first;
+  /** @var string $last The last name input */
+  public $last;
+  /** @var string $full The full name concatenated from first and last names */
+  public $full;
 
-    /**
-     * Method to show the full name
-     *
-     * @return void
-     */
-    public function showFullName() {
-        // Concatenate the first and last name and store in the $full variable
-        $this->full = $this->first . " " . $this->last;
+  /**
+   * Constructor to initialize the first and last name
+   *
+   * @param string $firstName
+   * @param string $lastName
+   * 
+   * @return void
+   */
+  public function __construct($firstName, $lastName) {
+    $this->first = $firstName;
+    $this->last = $lastName;
+  }
 
-        // Create a message that includes the full name
-        $message = "Hello " . $this->full;
+  /**
+   * Method to show the full name
+   *
+   * @return void
+   */
+  public function showFullName() {
+    // Concatenate the first and last name and store in the $full variable
+    $this->full = $this->first . " " . $this->last;
 
-        // Output the message to the user
-        echo $message;
-    }
+    // Create a message that includes the full name
+    $message = "Hello " . $this->full;
+
+    // Output the message to the user
+    echo $message;
+  }
 }
 
 // Check if the form has been submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_POST["first-name"];
-    $lastName = $_POST["last-name"];
+  $firstName = $_POST["first-name"];
+  $lastName = $_POST["last-name"];
 
-    // Create a new form instance 
-    $task = new Form($firstName, $lastName);
-   
-    // Make variable namePattern to match only alphabets
-    $namePattern = "/^[a-zA-Z]+$/";
-  
-    // Check if the first name and last name match the alphabets pattern
-    if (preg_match($namePattern, $firstName) && preg_match($namePattern, $lastName)) {
-        // If they match, show the full name using the method
-        $task->showFullName();
-    } else {
-        // If they don't match, display an error message
-        echo "Invalid input. Please enter only alphabets for first name and last name.";
-    }
+  // Create a new form instance 
+  $task = new Form($firstName, $lastName);
+
+  // Make variable namePattern to match only alphabets
+  $namePattern = "/^[a-zA-Z]+$/";
+
+  // Check if the first name and last name match the alphabets pattern
+  if (preg_match($namePattern, $firstName) && preg_match($namePattern, $lastName)) {
+    // If they match, show the full name using the method
+    $task->showFullName();
+  }
+  else {
+    // If they don't match, display an error message
+    echo "Invalid input. Please enter only alphabets for first name and last name.";
+  }
 }
 ?>
