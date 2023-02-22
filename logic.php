7934e35970a4f3d05fa23113f434d8f979a1627f
Gaurav gupta <Gaurav.gupta@innoraft.com>
2023-02-22 10:12:12 +0530

    FT2023-72: Add comments and space as per drupal coding standards in Task2

--- logic.php
+++ logic.php
@@ -1,63 +1,92 @@
 <?php
-// Define a class to represent the form
+
+/**
+ * Defines a class to represent the form.
+ */
 class Form {
-    // Public class properties
-    public $first, $last, $full, $filename, $filetemp;
+  /** @var string $first The first name input */
+  public $first;
+  /** @var string $last The last name input */
+  public $last;
+  /** @var string $full The full name concatenated from first and last names */
+  public $full;
+  /** @var string $filename The uploaded image filename.*/
+  public $filename;
+  /** @var string $filetemp The uploaded image temporary file path.*/
+  public $filetemp;
 
-    // Constructor to initialize the first and last name
-    public function __construct($firstName, $lastName, $file_name, $file_temp) {
-        $this->first = $firstName;
-        $this->last = $lastName;
-        $this->filename = $file_name;
-        $this->filetemp = $file_temp;
+  /**
+   * Constructor to initalize the form object
+   *
+   * @param string $firstName
+   * @param string $lastName
+   * @param string $fileName
+   * @param string $fileTemp
+   * 
+   * @return void
+   */
+  public function __construct($firstName, $lastName, $fileName, $fileTemp) {
+    $this->first = $firstName;
+    $this->last = $lastName;
+    $this->filename = $fileName;
+    $this->filetemp = $fileTemp;
+  }
+
+  /**
+   * Method to show the full name
+   *
+   * @return void
+   */
+  public function showFullName() {
+    // Concatenate the first and last name and store in the $full variable.
+    $this->full = $this->first . " " . $this->last;
+
+    // Create a message that includes the full name.
+    $message = "Hello " . $this->full;
+
+    // Output the message to the user.
+    echo $message;
+  }
+
+  /**
+   * Method to uploads the image.
+   * 
+   * @return void
+   */
+  public function uploadImage() {
+    // Validate if image is selected and not empty.
+    if (!empty($this->filename) && !empty($this->filetemp)) {
+      move_uploaded_file($this->filetemp, "upload-images/$this->filename");
     }
-
-    // Method to show the full name
-    public function showFullName() {
-        // Concatenate the first and last name and store in the $full variable
-        $this->full = $this->first . " " . $this->last;
-
-        // Create a message that includes the full name
-        $message = "Hello " . $this->full;
-
-        // Output the message to the user
-        echo $message;
-    }
-
-    // Function to upload the image
-    public function uploadImage() {
-        // Validate if image is selected and not empty
-        if (!empty($this->filename) && !empty($this->filetemp)) {
-            move_uploaded_file($this->filetemp, "upload-images/$this->filename");
-        }
-    }
+  }
 }
 
-// Check if the form has been submitted
+// Check if the form has been submitted.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Insert the value
-    $firstName = $_POST["first-name"];
-    $lastName = $_POST["last-name"];
-    $file_name = $_FILES['img']['name'];
-    $file_tmp = $_FILES['img']['tmp_name'];
+  // Insert the value.
+  $firstName = $_POST["first-name"];
+  $lastName = $_POST["last-name"];
+  $fileName = $_FILES['img']['name'];
+  $fileTemp = $_FILES['img']['tmp_name'];
 
-    // Make variable namePattern to match only alphabets
-    $namePattern = "/^[a-zA-Z]+$/";
+  // Make variable namePattern to match only alphabets.
+  $namePattern = "/^[a-zA-Z]+$/";
 
-    // Check if the first name and last name match the alphabets pattern
-    if (preg_match($namePattern, $firstName) && preg_match($namePattern, $lastName)) {
-        // Create a new form instance
-        $task = new Form($firstName, $lastName, $file_name, $file_tmp);
+  // Check if the first name and last name match the alphabets pattern.
+  if (preg_match($namePattern, $firstName) && preg_match($namePattern, $lastName)) {
+    // Create a new form instance.
+    $form = new Form($firstName, $lastName, $fileName, $fileTemp);
 
-        // If they match, show the full name using the method
-        $task->uploadImage();
+    // If they match, show the full name using the method.
+    $form->uploadImage();
 
-        // Print the image
-        echo "<p><img src='upload-images/$file_name' alt='img'></p>";
-        $task->showFullName();
-    } else {
-        // If they don't match, show an error message
-        echo "Invalid input. Please enter only alphabets for first name and last name.";
-    }
+    // Print the image.
+    echo "<p><img src='upload-images/$fileName' alt='img'></p>";
+    $form->showFullName();
+  } else {
+    // If they don't match, show an error message.
+    echo "Invalid input. Please enter only alphabets for first name and last name.";
+  }
 }
+
 ?>
